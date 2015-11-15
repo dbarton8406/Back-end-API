6838d0008f9065c83858ddd7aaa0eff59e378f5d
Daniel <dbarton8406@gmail.com>
2015-11-15 14:30:57 -0500

    added rescue to the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+    before_action :json_defaults
+
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render json: { error: "Could not find object: #{error.message}" },
+      status: :not_found
+  end
 
   def current_user
   	token = request.headers["Access-Token"]
