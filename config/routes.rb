72c7d657830a69ab4bc16b13e60f9eb52ebe550f
Charles <codprestige10@gmail.com>
2015-11-15 14:07:22 -0500

    debugged edit and delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
 post "/treasurers", to: "treasurers#create"
 post "/treasurers/login", to: "treasurers#login"
 get "treasurers/:id", to: "treasurers#index"
-post "/treasurers/destroy/:id", to: "treasurers#destroy"
+delete "/treasurers/destroy/:id", to: "treasurers#destroy"
 
 post "/bills/create", to: "bills#create"
 get "/bills/:treasurer_id", to: "bills#index"
 get "bills/assignee/:assignee", to: "bills#assignee"
 get "/bills/bill/:id", to: "bills#show"
 put "/bills/:id", to: "bills#update"
-post "/bills/destroy/:id", to: "bills#destroy"
+delete "/bills/destroy/:id", to: "bills#destroy"
 
 get "/roomates/:treasurer_id", to: "roomates#index"
 post "/roomates/login", to: "roomates#login"
 post "/roomates", to: "roomates#create"
-post "/roomates/destroy/:id", to: "roomates#destroy"
+delete "/roomates/destroy/:id", to: "roomates#destroy"
 
 
 
